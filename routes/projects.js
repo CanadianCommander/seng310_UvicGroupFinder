0480b75e2cfb3e4c2923fd72cff6f952f365d6e6
Benjamin Benetti <ben.benetti@shaw.ca>
2017-07-16 17:27:38 -0700

    1. add student project select page. allows students to navigate to the 	group listings for a specific project. 2. add link to student project select page in the main_hub

--- routes/projects.js
+++ routes/projects.js
@@ -4,9 +4,17 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function (req, res, next ){
+router.get('/create', function (req, res, next ){
   res.render('project_creation_page');
 });
-
+router.get('/select', function (req, res, next ){
+  res.render('project_select_page',
+    {projects: [
+      {name: "SENG 310 Main Project"},
+      {name: "Friday Slides Project"},
+      {name: "Lab Mini Project"}
+    ]}
+  );
+});
 
 module.exports = router;
